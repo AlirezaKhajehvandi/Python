21a0cf089aae334fd1913ec90d43c0e73c7b3c15
Alireza Khajehvandi <alireza76khajehvandi@gmail.com>
2023-04-13 22:52:54 +0330

    calculating duration for mp4 files in the specific path

--- Calculate_duration_mp4/src/main.py
+++ Calculate_duration_mp4/src/main.py
@@ -1,17 +1,32 @@
 from pathlib import Path
+import moviepy
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
-desktop = Path("/mnt/windows/Tutorials/Course/UDEMY/Python Django Dev To Deployment")
-print ("alireza khajehvnadi")
-print (Path.cwd())
 
-for filename in Path(path2).glob('*.mp4'):
+print (moviepy.__version__)
+mp4_file = Path("/mnt/windows/Tutorials/Course/UDEMY/Python Django Dev To Deployment")
+
+# variable for total time in seconds, minutes, and hours
+total_time_second = 0
+total_time_minute = 0
+total_time_hour = 0
+
+for count, filename in enumerate(Path(mp4_file).glob('*.mp4'), 1):
     clip = VideoFileClip(filename.as_posix())
-    print(clip.duration)
+    total_time_second += clip.duration
+    # print (clip.duration, count)
+
+total_time_minute = total_time_second / 60
+total_time_hour = total_time_minute / 60
+
+print (f"total time in second: {total_time_second}")
+print (f"total time in minute: {total_time_minute}")
+print (f"total time in hour: {total_time_hour}")
+    
 
 
-mp4_file = list(desktop.glob("*.mp4"))
-# print (mp4_file)
-print (type(mp4_file))
-for file in  mp4_file:
-    print(str(file).split("/")[-1])
+# mp4_file = list(desktop.glob("*.mp4"))
+# # print (mp4_file)
+# print (type(mp4_file))
+# for file in  mp4_file:
+#     print(str(file).split("/")[-1])
