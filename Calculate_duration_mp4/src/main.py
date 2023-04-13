f87b2e5949f72a5781110e68d512ed12b3bc7d43
Alireza Khajehvandi <alireza76khajehvandi@gmail.com>
2023-04-13 23:10:38 +0330

    clean code and readable

--- Calculate_duration_mp4/src/main.py
+++ Calculate_duration_mp4/src/main.py
@@ -1,32 +1,29 @@
+# importing necessary modules
 from pathlib import Path
-import moviepy
+from tqdm.auto import tqdm
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
-
-print (moviepy.__version__)
-mp4_file = Path("/mnt/windows/Tutorials/Course/UDEMY/Python Django Dev To Deployment")
+# Choose yout path (put your path instead of 'your path')
+mp4_file = Path("your path")
 
 # variable for total time in seconds, minutes, and hours
 total_time_second = 0
 total_time_minute = 0
 total_time_hour = 0
 
-for count, filename in enumerate(Path(mp4_file).glob('*.mp4'), 1):
+# looping to get the duration of any file
+for count, filename in tqdm(enumerate(Path(mp4_file).glob('*.mp4'), 1)):
     clip = VideoFileClip(filename.as_posix())
     total_time_second += clip.duration
     # print (clip.duration, count)
 
+# calculating duration for mp4 files in the specific path
 total_time_minute = total_time_second / 60
 total_time_hour = total_time_minute / 60
 
-print (f"total time in second: {total_time_second}")
-print (f"total time in minute: {total_time_minute}")
-print (f"total time in hour: {total_time_hour}")
+# showing results
+print (f"total time in second: {total_time_second} seconds")
+print (f"total time in minute: {total_time_minute} minutes")
+print (f"total time in hour: {total_time_hour} hours")
+print (f"number of files: {count}")
     
-
-
-# mp4_file = list(desktop.glob("*.mp4"))
-# # print (mp4_file)
-# print (type(mp4_file))
-# for file in  mp4_file:
-#     print(str(file).split("/")[-1])
